3df23ac241f2c13bd075820c1e7e2832df8473e6
Luis_Fellipe(Proplayer:)) <luis.fellipe@escolar.ifrn.edu.br>
2024-11-14 15:58:07 -0300

    Desenvolvendo login

--- src/main/java/Sports/JLMN/Controllers/sportsController.java
+++ src/main/java/Sports/JLMN/Controllers/sportsController.java
@@ -1,13 +1,19 @@
 package Sports.JLMN.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class sportsController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/home")
 	public String site() {
 		return "home";
+	}
+	
+	@GetMapping("/")
+	public String login() {
+		return "login";
 	}
 }
