9a70e48b27f4fda174609cfa9b48ef2152c1c763
Roland-D5V <luisfellipericardolisboa@gmail.com>
2024-11-22 23:07:58 -0300

    Inserindo produtos no BD

--- src/main/java/Sports/JLMN/Controllers/sportsController.java
+++ src/main/java/Sports/JLMN/Controllers/sportsController.java
@@ -1,18 +1,39 @@
 package Sports.JLMN.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import Sports.JLMN.models.Produto;
+import Sports.JLMN.repositories.produtoRepository;
+
 @Controller
+@RequestMapping("/JLMN")
 public class sportsController {
 	
-	@RequestMapping("/home")
-	public String site() {
+	@Autowired
+	private produtoRepository pr;
+	
+	@GetMapping("/home")
+	public String home() {
 		return "home";
 	}
 	
-	@GetMapping("/")
+	@GetMapping("/addProduto")
+	public String addProduto(Produto produto) {
+		return "/addProduto";
+	}
+	
+	@PostMapping("/saveProduto")
+	public String saveProduto(Produto produto) {
+		System.out.println(produto.toString());
+		pr.save(produto);
+		return "redirect:/JLMN/home";
+	}
+	
+	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
---
+++ src/main/java/Sports/JLMN/repositories/produtoRepository.java
@@ -0,0 +1,9 @@
+package Sports.JLMN.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import Sports.JLMN.models.Produto;
+
+public interface produtoRepository extends JpaRepository<Produto, Long> {
+
+}
