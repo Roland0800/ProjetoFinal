81f74a871ee4593fc6cccb8c72284c50d780c730
Roland-D5V <luisfellipericardolisboa@gmail.com>
2024-12-04 23:40:57 -0300

    senha terminada

--- src/main/java/Sports/JLMN/repositories/usuarioRepository.java
+++ src/main/java/Sports/JLMN/repositories/usuarioRepository.java
@@ -1,15 +1,17 @@
 package Sports.JLMN.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import Sports.JLMN.models.Usuario;
+
 
 public interface usuarioRepository extends JpaRepository<Usuario, Long> {
 
-	@Query("select i from Usuario i where i.nome = :nome and i.senha = :senha and i.tipo = :tipo")
-	public Usuario login(String nome, String senha, String tipo);
+	@Query("select i from Usuario i where i.nome = :nome and i.senha = :senha and i.email = :email and i.tipo = :tipo")
+	public Usuario login(String nome, String senha, String email, String tipo);
 	
-	@Query("update i Usuario i set i.senha i where i.id = :id")
-	public Usuario newSenha(String senha, Long id);
+	public Optional<Usuario> getByEmail(String email);
+	
 }
